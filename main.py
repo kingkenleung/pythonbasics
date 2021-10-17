306891cc0fce489a00e6ee678a6f3f5c1035d506
kingkenleung <30319344+kingkenleung@users.noreply.github.com>
2021-10-17 23:23:21 +0800

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+def fib():
+    a = 0
+    b = 1
+    
+    basic.show_number(a)
+    basic.show_number(b)
+    for k in range(30):
+        c = a + b
+        basic.show_number(c)
+        a = b
+        b = c
 
+basic.forever(fib)
--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+basic.forever(function fib() {
+    let c: number;
+    let a = 0
+    let b = 1
+    basic.showNumber(a)
+    basic.showNumber(b)
+    for (let k = 0; k < 30; k++) {
+        c = a + b
+        basic.showNumber(c)
+        a = b
+        b = c
+    }
+})
